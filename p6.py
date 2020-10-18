54a6c2eb63eb7eeff577cdcb5923bf4d11c08b8d
steven-rr <steverelativity@gmail.com>
2020-10-18 06:54:36 -0400

    finalize plotting for p6.

--- p6.py
+++ p6.py
@@ -1,52 +1,62 @@
-import numpy as np
+# ////////////////////////////////////////////////////////////////////////////
+# //|
+# //| Author : Steven Rivadeneira
+# //|
+# //| File Name : p6.py
+# //|
+# //| Description : Graphs linear transformation.
+# //|
+# //| Notes :
+# //|
+# //|
+# //|
+# //|
+# ////////////////////////////////////////////////////////////////////////////
+
 from math import cos
 from math import sin
 from math import pi
+from math import sqrt
 import matplotlib.pyplot as plt
 
-# (2x + y)
-#   (y)
+def linear_transform(x, y):
+        result =  [2*x + y, 3*y]
+        return result
 
-# (2 1) (x) =  (2x + y)
-# (0 1) (y)       (y)
-LTx_plus = []
-LTy_plus= []
+def main():
+        LTx_plus = []
+        LTy_plus= []
 
-LTx_minus = []
-LTy_minus = []
-for theta in range(0, 101, 1):
-        #change step size
-        theta = pi*(theta/100)
-        #calc
-        LTx_plus.append(2*cos(theta) + sin(theta))
-        LTy_plus.append(sin(theta))
+        LTx_minus = []
+        LTy_minus = []
+        for theta in range(0, 101, 1):
+                #change step size
+                theta = pi*(theta/100)
+                #calc
+                LT = linear_transform(cos(theta),sin(theta))
+                LTx_plus.append(LT[0])
+                LTy_plus.append(LT[1])
 
-for theta in range(0, 101, 1):
-        # change step size.
-        theta = pi + pi*(theta/100)
-        #calc
-        LTx_minus.append(2*cos(theta) + sin(theta))
-        LTy_minus.append(sin(theta))
+        for theta in range(0, 101, 1):
+                # change step size.
+                theta = pi + pi*(theta/100)
+                #calc
+                LT = linear_transform(cos(theta),sin(theta))
+                LTx_minus.append(LT[0])
+                LTy_minus.append(LT[1])
 
-#plot
-plt.figure(1)
-plt.plot(LTx_plus, LTy_plus, 'b')
-plt.plot(LTx_minus, LTy_minus, 'r')
-plt.show()
-
-# # V = aA  +  bB
-# # current problem:
-# # 3 = 2a  +  3b
-# # 5 =  a  + -2b
-#
-# V = np.array([4,11])
-# A = np.array([7,13])
-# B = np.array([3,-2])
-#
-# n = np.cross(V,B)
-# a = np.dot(np.cross(B,V), n) #/ np.dot(np.cross(B,A),g)
-# a_cramer = (B[1]*V[0] - B[0]*V[1]) / (A[0]*B[1] - A[1]*B[0])
-# b_cramer = (A[0]*V[1] - A[1]*V[0]) / (A[0]*B[1] - A[1]*B[0])
+        #calc values.
+        point1 = linear_transform(1,0)
+        point2 = linear_transform(1/sqrt(2), 1/sqrt(2))
+        #plot
+        plt.figure(1)
+        plt.plot(LTx_plus, LTy_plus, 'b')
+        plt.plot(LTx_minus, LTy_minus, 'r')
+        plt.plot(point1[0], point1[1] , 'ro', label = "point (1,0)")
+        plt.plot(point2[0], point2[1] , 'bo', label = "point (1/sqrt2, 1/sqrt2)")
+        plt.legend(loc="upper left")
+        plt.show()
 
 
-######
+if __name__ == "__main__":
+        main()
